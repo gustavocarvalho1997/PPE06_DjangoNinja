b7e1babfad93a12b5b60cf4667c72ac77b6f238f
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-16 18:37:38 -0300

    implementa o listar alunos

--- treino/api.py
+++ treino/api.py
@@ -1,11 +1,12 @@
 from ninja import Router
 from ninja.errors import HttpError
+from typing import List
 from treino.schemas import AlunoSchema
 from treino.models import Aluno
 
 treino_router = Router()
 
-@treino_router.post("alunos/", response={200: AlunoSchema, 400: str})
+@treino_router.post("alunos/", response={200: AlunoSchema, 400: str}, description="Cadastrar um aluno")
 def criar_aluno(request, aluno_schema: AlunoSchema):
     nome = aluno_schema.dict()['nome']
     email = aluno_schema.dict()['email']
@@ -25,3 +26,8 @@ def criar_aluno(request, aluno_schema: AlunoSchema):
     aluno.save()
 
     return aluno
+
+@treino_router.get("alunos/", response={200: List[AlunoSchema]}, description="Listar todos os alunos")
+def listar_alunos(request):
+    alunos = Aluno.objects.all()
+    return alunos
