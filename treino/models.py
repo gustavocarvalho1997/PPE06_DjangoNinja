992c7dab49295ba2c697f07dca88f32d0094c001
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-16 19:42:44 -0300

    criado model aula concluida

--- treino/models.py
+++ treino/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Aluno(models.Model):
-    faixa_choices = (
+faixa_choices = (
         ('B', 'Branca'),
         ('A', 'Azul'),
         ('R', 'Roxa'),
         ('M', 'Marrom'),
         ('P', 'Preta')
     )
+
+class Aluno(models.Model):
+    
 
     nome = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
@@ -17,3 +19,11 @@ class Aluno(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class AulaConcluida(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    data = models.DateField(auto_now_add=True)
+    faixa_atual = models.CharField(max_length=1, choices=faixa_choices, default='B')
+
+    def __str__(self):
+        return self.aluno.nome
