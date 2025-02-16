3609c4463874dbaa9f25ddac42447630bcc096d4
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-16 20:22:39 -0300

    implementado endpoint com info do progresso do aluno

--- treino/api.py
+++ treino/api.py
@@ -1,8 +1,9 @@
 from ninja import Router
 from ninja.errors import HttpError
 from typing import List
-from treino.schemas import AlunoSchema
-from treino.models import Aluno
+from treino.schemas import AlunoSchema, ProgressoAlunoSchema
+from treino.models import Aluno, AulaConcluida
+from treino.graduacao import calculate_lesson_to_upgrade, order_belt
 
 treino_router = Router()
 
@@ -31,3 +32,23 @@ def criar_aluno(request, aluno_schema: AlunoSchema):
 def listar_alunos(request):
     alunos = Aluno.objects.all()
     return alunos
+
+@treino_router.get("/progresso_aluno/", response={200: ProgressoAlunoSchema}, description="Verificar progresso do aluno")
+def progresso_aluno(request, email_aluno: str):
+    aluno = Aluno.objects.get(email=email_aluno)
+    if not aluno:
+        raise HttpError(404, "Aluno não encontrado")
+    faixa_atual = aluno.get_faixa_display() # Retorna o valor da faixa, não a sigla
+    n = order_belt.get(faixa_atual, 0) # Retorna o valor da faixa, se não existir retorna 0
+    total_aulas_proxima_faixa = calculate_lesson_to_upgrade(n)
+    total_aulas_concluidas_faixa = AulaConcluida.objects.filter(aluno=aluno, faixa_atual=aluno.faixa).count() 
+    aulas_faltantes = total_aulas_proxima_faixa - total_aulas_concluidas_faixa
+
+    return {
+        "email": aluno.email,
+        "nome": aluno.nome,
+        "faixa": faixa_atual,
+        "total_aulas": total_aulas_concluidas_faixa,
+        "aulas_necessarias_para_proxima_faixa": aulas_faltantes
+    }
+    
